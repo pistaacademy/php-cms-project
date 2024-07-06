931dfedc9656148c633f3be351f6a8522e8d287b
pistaacademy <pista.academy@gmail.com>
2024-07-06 06:08:08 +0330

    Session-31

--- admin/includes/edit_user.php
+++ admin/includes/edit_user.php
@@ -1,104 +1,89 @@
-<?php
+<?php 
 
-if(isset($_GET['p_id'])){
-    $the_post_id = $_GET['p_id'];
-}
+    if(isset($_GET['edit_user'])) {
+        $the_user_id = $_GET['edit_user'];
 
-$query = "SELECT * FROM posts WHERE post_id = $the_post_id";
-$select_posts = mysqli_query($connection, $query);
-while($row = mysqli_fetch_assoc($select_posts)){
-    $post_id = $row['post_id'];
-    $post_author = $row['post_author'];
-    $post_title = $row['post_title'];
-    $post_category_id = $row['post_category_id'];
-    $post_status = $row['post_status'];
-    $post_image = $row['post_image'];
-    $post_tags = $row['post_tags'];
-    $post_comment_count = $row['post_comment_count'];
-    $post_date = $row['post_date'];
-}
-
-if(isset($_POST['update_post'])) {
-
-    $post_title = $_POST['post_title'];
-    $post_category_id = $_POST['post_category'];
-    $post_author = $_POST['post_author'];
-    $post_status = $_POST['post_status'];
-    $post_image = $_FILES['post_image']['name'];
-    $post_image_temp = $_FILES['post_image']['tmp_name'];
-    $post_tags = $_POST['post_tags'];
-    $post_content = $_POST['post_content'];
-
-    move_uploaded_file($post_image_temp, "D:/Pista Academy/PHP/cms/admin/images/$post_image");
-
-    if(empty($post_image)) {
-        $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
-        $select_image = mysqli_query($connection, $query);
-        while($row = mysqli_fetch_assoc($select_image)) {
-            $post_image = $row['post_image'];
-        }
+        $query = "SELECT * FROM users WHERE user_id = $the_user_id";
+        $select_users_query = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_assoc($select_users_query)){
+            $user_id = $row['user_id'];
+            $username = $row['username'];
+            $user_password = $row['user_password'];
+            $user_firstname = $row['user_firstname'];
+            $user_lastname = $row['user_lastname'];
+            $user_email = $row['user_email'];
+            $user_image = $row['user_image'];
+            $user_role = $row['role'];
+    }
     }
 
-    $query = "UPDATE posts SET ";
-    $query .="post_title = '{$post_title}', ";
-    $query .="post_category_id = '{$post_category_id}', ";
-    $query .="post_date = now(), ";
-    $query .="post_author = '{$post_author}', ";
-    $query .="post_status = '{$post_status}', ";
-    $query .="post_tags = '{$post_tags}', ";
-    $query .="post_content = '{$post_content}', ";
-    $query .="post_image = '{$post_image}' ";
-    $query .= "WHERE post_id = {$the_post_id} ";
+    if(isset($_POST['create_user'])) {
 
-    $update_post = mysqli_query($connection, $query);
+        $user_firstname = $_POST['user_firstname'];
+        $user_lastname = $_POST['user_lastname'];
+        $user_role = $_POST['user_role'];
 
-    confirmQuery($update_post);
-    header("Location: posts.php");
-}
+        // $post_image = $_FILES['post_image']['name'];
+        // $post_image_temp = $_FILES['post_image']['tmp_name'];
+
+        $username = $_POST['username'];
+        $user_email = $_POST['user_email'];
+        $user_password = $_POST['user_password'];
+        // $post_date = date('d-m-y');
+
+
+        // move_uploaded_file($post_image_temp, "D:/Pista Academy/PHP/cms/admin/images/$post_image");
+
+        $query = "INSERT INTO users(user_firstname,user_lastname,role, username, user_email, user_password) ";
+
+        $query .= "VALUES('{$user_firstname}', '{$user_lastname}', '{$user_role}', '{$username}', '{$user_email}', '{$user_password}')";
+
+        $create_user_query = mysqli_query($connection, $query);
+
+        confirmQuery($create_user_query);
+        header("Location: users.php");
+    }
 ?>
 
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
-        <label for="post_title">Post Title</label>
-        <input value="<?php echo $post_title ?>" type="text" class="form-control" name="post_title">
+        <label for="user_firstname">Firstname</label>
+        <input type="text" value="<?php echo $user_firstname ?>" class="form-control" name="user_firstname">
     </div>
     <div class="form-group">
-        <select name="post_category" id="">
-            <?php 
-                $query = "SELECT * FROM categories";
-                $select_categories = mysqli_query($connection, $query);
-                confirmQuery($select_categories);
-                while($row = mysqli_fetch_assoc($select_categories)) {
-                    $cat_id = $row['cat_id'];
-                    $cat_title = $row['cat_title'];
-
-                    echo "<option value='{$cat_id}'>{$cat_title}</option>";
-                }
-            ?>
+        <label for="user_lastname">Lastname</label>
+        <input type="text" value="<?php echo $user_lastname ?>" class="form-control" name="user_lastname">
+    </div>
+    <div class="form-group">
+        <select name="user_role" id="">
+            <option value="subscriber">Select Options</option>
+            <option value="admin">Admin</option>
+            <option value="subscriber">Subscriber</option>
         </select>
     </div>
     <div class="form-group">
-        <label for="post_author">Post Author</label>
-        <input value="<?php echo $post_author ?>" type="text" class="form-control" name="post_author">
+        <label for="username">Username</label>
+        <input type="text" value="<?php echo $username ?>" class="form-control" name="username">
     </div>
     <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input value="<?php echo $post_status ?>" type="text" class="form-control" name="post_status">
+        <label for="user_email">Email</label>
+        <input type="email" value="<?php echo $user_email ?>" class="form-control" name="user_email">
     </div>
     <div class="form-group">
-        <img width="100" src="../images/<?php echo $post_image; ?>" alt="<?php echo $post_title; ?>">
-        <input type="file" name="post_image">
+        <label for="user_password">Password</label>
+        <input type="password" value="<?php echo $user_password ?>" class="form-control" name="user_password">
     </div>
+
+    <!-- <div class="form-group">
+        <label for="post_category_id">Post Category ID</label>
+        <input type="text" class="form-control" name="post_category_id">
+    </div> -->
+
+    
+
+
     <div class="form-group">
-        <label for="post_tags">Post Tags</label>
-        <input value="<?php echo $post_tags ?>" type="text" class="form-control" name="post_tags">
-    </div>
-    <div class="form-group">
-        <label for="post_content">Post Content</label>
-        <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_title ?></textarea>
-    </div>
-    <div class="form-group">
-        <input type="submit" class="btn btn-primary" name="update_post" value="Update Post">
+        <input type="submit" class="btn btn-primary" name="edit_user" value="Edit User">
     </div>
 </form>
--- admin/includes/view_all_users.php
+++ admin/includes/view_all_users.php
@@ -50,8 +50,9 @@
                                         // }
                                         
                                         
-                                        echo "<td><a href='users.php?approve=$user_id'>Approve</a></td>";
-                                        echo "<td><a href='users.php?unapprove=$user_id'>UnApprove</a></td>";
+                                        echo "<td><a href='users.php?change_to_admin=$user_id'>Admin</a></td>";
+                                        echo "<td><a href='users.php?change_to_sub=$user_id'>Subscriber</a></td>";
+                                        echo "<td><a href='users.php?source=edit_user&edit_user={$user_id}'>Edit</a></td>";
                                         echo "<td><a href='users.php?delete=$user_id'>Delete</a></td>";
                                         echo "</tr>";
                                     }
@@ -62,18 +63,18 @@
 
 <?php 
 
-if(isset($_GET['unapprove'])){
-    $the_comment_id = $_GET['unapprove'];
-    $query = "UPDATE comments SET comment_status = 'unapprove' WHERE comment_id = {$the_comment_id}";
-    $unapprove_comment_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+if(isset($_GET['change_to_admin'])){
+    $the_user_id = $_GET['change_to_admin'];
+    $query = "UPDATE users SET role = 'admin' WHERE user_id = {$the_user_id}";
+    $change_to_admin_query = mysqli_query($connection, $query);
+    header("Location: users.php");
 }
 
-if(isset($_GET['approve'])){
-    $the_comment_id = $_GET['approve'];
-    $query = "UPDATE comments SET comment_status = 'approve' WHERE comment_id = {$the_comment_id}";
-    $approve_comment_query = mysqli_query($connection, $query);
-    header("Location: comments.php");
+if(isset($_GET['change_to_sub'])){
+    $the_user_id = $_GET['change_to_sub'];
+    $query = "UPDATE users SET role = 'subscriber' WHERE user_id = {$the_user_id}";
+    $change_to_sub_query = mysqli_query($connection, $query);
+    header("Location: users.php");
 }
 
 if(isset($_GET['delete'])){
