1d779e21c4f50950432982be0b231b73a6f36ac8
pistaacademy <pista.academy@gmail.com>
2024-07-08 06:17:40 +0330

    Session-34

--- includes/login.php
+++ includes/login.php
@@ -1,4 +1,5 @@
 <?php include "db.php"; ?>
+<?php session_start(); ?>
 
 <?php 
 
@@ -15,7 +16,26 @@
             die("Query Failed". mysqli_error($connection));
         }
         while($row = mysqli_fetch_array($select_user_query)){
-            echo $db_id = $row['user_id'];
+            $db_user_id = $row['user_id'];
+            $db_username = $row['username'];
+            $db_user_password = $row['user_password'];
+            $db_user_firstname = $row['user_firstname'];
+            $db_user_lastname = $row['user_lastname'];
+            $db_user_role = $row['role'];
+        }
+       
+        if ($username !== $db_username && $password !== $db_user_password) {
+            header("Location: ../index.php");
+        } else if ($username == $db_username && $password == $db_user_password) {
+            
+            $_SESSION['username'] = $db_username;
+            $_SESSION['firstname'] = $db_user_firstname;
+            $_SESSION['lastname'] = $db_user_lastname;
+            $_SESSION['user_role'] = $db_user_role;
+
+            header("Location: ../admin/index.php");
+        } else {
+            header("Location: ../index.php");
         }
     }
 ?>
