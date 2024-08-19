c4921b87ed76475aa7e17af19b3f6dc1454d77ed
pistaacademy <pista.academy@gmail.com>
2024-08-19 05:39:45 +0330

    Session-52

--- author_post.php
+++ author_post.php
@@ -14,22 +14,22 @@
             <div class="col-md-8">
 
             <?php 
-                        $query = "SELECT * FROM posts";
+                        if(isset($_GET['p_id'])){
+                            $the_post_id = $_GET['p_id'];
+                            $the_post_author = $_GET['author'];
+                        }
+
+                        $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}'";
                         $select_all_posts_query = mysqli_query($connection, $query);
                         while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                            $post_id = $row['post_id'];
                             $post_title = $row['post_title'];
                             $post_author = $row['post_author'];
                             $post_date = $row['post_date'];
                             $post_image = $row['post_image'];
-                            $post_content = substr($row['post_content'],0,200);
-                            $post_status = $row['post_status'];
+                            $post_content = $row['post_content'];
 
-                            if($post_status == 'published'){
-
-                               
                             ?>
-                            
+
                             <h1 class="page-header">
                             Page Heading
                             <small>Secondary Text</small>
@@ -37,24 +37,19 @@
         
                         <!-- First Blog Post -->
                         <h2>
-                            <a href="post.php?p_id=<?php echo $post_id ?>"><?php echo $post_title ?></a>
+                            <a href="#"><?php echo $post_title ?></a>
                         </h2>
                         <p class="lead">
-                            by <a href="author_post.php?author=<?php echo $post_author; ?>&p_id=<?php echo $post_id; ?>"><?php echo $post_author ?></a>
+                            All Post by <?php echo $post_author ?>
                         </p>
                         <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                         <hr>
-                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">
                         <img class="img-responsive" src="admin/images/<?php echo $post_image ?>" alt="">
-                        </a>
                         <hr>
                         <p><?php echo $post_content ?></p>
-                        <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
         
                         <hr>
-                       <?php }} ?>
-
-               
+                       <?php } ?>
             </div>
 
             <!-- Blog Sidebar Widgets Column -->
